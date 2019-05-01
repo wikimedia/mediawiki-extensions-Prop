91920d5dfe448b8cd7fefc38f99414c1c92e6b65
Sophivorus <schenonef@gmail.com>
2019-05-01 11:10:21 -0300

    Initial commit
    Change-Id: If0ffe1514b9c09aad9f596a4c39b09a38d7fb208

---
+++ Prop.alias.php
@@ -0,0 +1,11 @@
+<?php
+
+$magicWords = [];
+
+$magicWords['en'] = [
+   'Prop' => [ 0, 'Prop' ],
+];
+
+$magicWords['es'] = [
+   'Prop' => [ 0, 'Prop' ],
+];
---
+++ Prop.php
@@ -0,0 +1,29 @@
+<?php
+
+class Prop {
+
+	/**
+	 * Main hook
+	 *
+	 * @param Parser &$parser Parser object
+	 */
+	public static function onParserFirstCallInit( Parser &$parser ) {
+		$parser->setFunctionHook( 'Prop', 'Prop::onFunctionHook' );
+	}
+
+	/**
+	 * Parser function hook
+	 *
+	 * @param Parser $parser Parser object
+	 * @param string $name Name of the property
+	 * @param string $value Value of the property
+	 */
+	public static function onFunctionHook( Parser &$parser, $name, $value = null ) {
+		$output = $parser->getOutput();
+		if ( $value ) {
+        	$output->setProperty( $name, $value );
+		} else {
+			return $output->getProperty( $name );
+		}
+	}
+}
